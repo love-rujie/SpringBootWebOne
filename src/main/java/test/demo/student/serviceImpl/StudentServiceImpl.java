778f3613310bc6d408cb66a7ffeaf0a2eceb4319
C00674 <wangrujie1996@icloud.com>
2019-04-13 00:50:41 +0800

    success

--- src/main/java/test/demo/student/serviceImpl/StudentServiceImpl.java
+++ src/main/java/test/demo/student/serviceImpl/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package test.demo.student.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import test.demo.student.dao.StudentDao;
 import test.demo.student.pojo.Student;
 import test.demo.student.service.StudentService;
 import org.springframework.stereotype.Service;
@@ -7,8 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentDao studentDao;
     @Override
     public List<Student> findLAll() {
-        return null;
+        List<Student> studentList = studentDao.select();
+        return studentList;
     }
 }
